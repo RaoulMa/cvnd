d7cc43bf382c7a07b708718ffe7d08611024e94f
Raoul Malm <raoul.malm@gmail.com>
2019-05-12 17:41:09 +0200

    refactor: Code

--- dsnd/python_package/3_upload_to_pypi/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='distributionsraoulmalm',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['distributionsraoulmalm'],
-      zip_safe=False)
